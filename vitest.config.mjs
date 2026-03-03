7067fff4cb18ab8d8fc4913034b723e236728381
Pablo Perez Hernandez <alu0101619002@ull.edu.es>
2026-03-03 23:03:37 +0000

    Coverge actualization

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
+    environment: 'node',
+    // Esto asegura que Vitest busque los archivos .spec.ts en la carpeta test
     include: ['test/**/*.spec.ts'], 
-    globals: true
+    coverage: {
+      provider: 'v8', 
+      // Aquí indicamos que queremos medir la cobertura de TODO lo que esté en src
+      include: ['src/**/*.ts'],
+      // Excluimos archivos que no contienen lógica (como el index.ts raíz)
+      exclude: ['src/index.ts', 'src/**/index.ts'],
+      reporter: ['text', 'html'],
+      all: true, // Esto obliga a incluir archivos incluso si no tienen tests asociados
+    },
   },
 });
